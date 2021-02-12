8d084828a2f8f3e0a49ebcadc3a0e4f04e84cffc
daoc <diego.ordonez@gmail.com>
2021-02-12 17:56:10 -0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cuda_AddArrays_UM
+Añade 2 arreglos. Hecho en VB19, Win10
+Utiliza Unified Shared Memory (USM)
---
+++ kernel.cu
@@ -0,0 +1,45 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__
+void addArrays(int* A, int* B, int* C) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	C[i] = A[i] + B[i];
+}
+
+int main(void) {
+	int N = 1024;
+
+	int *A, *B, *C;
+
+	// Crea los buffer, con Unified Memory, para los datos de entrada y salida
+	cudaMallocManaged(&A, N * sizeof(int));
+	cudaMallocManaged(&B, N * sizeof(int));
+	cudaMallocManaged(&C, N * sizeof(int));
+
+	// Inicializa los buffer del host con los valores de entrada
+	for (int i = 0; i < N; i++) {
+		A[i] = i; //0,1,2,...,1023
+		B[i] = N - i; //1023,1022,...,0
+	}
+
+	// Ejecuta la kernel en la GPU (4 bloques * 256 hilos = 1024 elementos calculados)
+	addArrays <<<4, 256>>> (A, B, C);
+
+	// Espera que termine la kernel
+	cudaDeviceSynchronize();
+
+	// Presenta el resultado
+	for (int i = 0; i < N; i++) {
+		printf("Resultados %d: (%d + %d = %d)\n", i, A[i], B[i], C[i]);
+	}
+
+	// Libera los recursos
+	cudaFree(A);
+	cudaFree(B);
+	cudaFree(C);
+
+}
